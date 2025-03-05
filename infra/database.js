3a4666f18fabd0657c247b32a921768549b0402e
Judhagsan <judhagsan@gmail.com>
2025-03-05 15:06:26 -0300

    adicionar SSL configurado

--- infra/database.js
+++ infra/database.js
@@ -8,7 +8,7 @@ async function query(queryObeject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -26,3 +26,13 @@ async function query(queryObeject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
