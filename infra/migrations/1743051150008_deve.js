a0c5d5e033afd73cb979f7ac426b00c9f21913d8
judhagsan <judhagsan@gmail.com>
2025-07-03 19:49:16 -0300

    feat: add avisado migration

--- infra/migrations/1743051150008_deve.js
+++ infra/migrations/1743051150008_deve.js
@@ -9,7 +9,6 @@ exports.up = (pgm) => {
     valorpapel: { type: "numeric", notNull: true },
     valorcomissao: { type: "numeric", notNull: true },
     valor: { type: "numeric", notNull: true },
-    avisado: { type: "numeric", notNull: true, default: 0 },
   });
 };
 
